ecad8d81ad532a4aa4a4045bb3d67c401ba7effe
SonyaV <33463478+SonyaV@users.noreply.github.com>
2022-03-10 11:22:30 +0100

    add tests

---
+++ tests/org.palladiosimulator.view.plantuml.test/src/org/palladiosimulator/view/plantuml/test/SystemTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package org.palladiosimulator.view.plantuml.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author HP
+ *
+ */
+class SystemTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
